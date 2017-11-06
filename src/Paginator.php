b2915f30cd059aa81df52916b7248bf51a24e61a
Unknown <railken@protonmail.ch>
2017-11-06 17:32:34 +0100

    remove count from paginator

--- src/Paginator.php
+++ src/Paginator.php
@@ -7,15 +7,4 @@ use Railken\ApiHelpers\Paginator as BasePaginator;
 class Paginator extends BasePaginator
 {
 
-	/**
-	 * Count records query
-	 *
-	 * @param $query
-	 *
-	 * @return integer
-	 */
-	public function count($query)
-	{
-		return $query->count();
-	}
 }
