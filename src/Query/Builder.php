6cf449e4a81beb48eba4fb526139c434f9f3eb22
Unknown <railken@protonmail.ch>
2018-03-19 17:01:16 +0100

    cleanup

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -43,23 +43,4 @@ class Builder
             $visitor->visit($query, $node, $context);
         }
     }
-
-    public function generateCallTrace()
-{
-    $e = new \Exception();
-    $trace = explode("\n", $e->getTraceAsString());
-    // reverse array to make steps line up chronologically
-    $trace = array_reverse($trace);
-    array_shift($trace); // remove {main}
-    array_pop($trace); // remove call to this method
-    $length = count($trace);
-    $result = array();
-    
-    for ($i = 0; $i < $length; $i++)
-    {
-        $result[] = ($i + 1)  . ')' . substr($trace[$i], strpos($trace[$i], ' ')); // replace '#someNum' with '$i)', set the right ordering
-    }
-    
-    return "\t" . implode("\n\t", $result);
-}
 }
